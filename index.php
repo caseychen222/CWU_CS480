9ad472fdd8ac373105cceb9b88611cd8cd166a17
htmlcc92 <casey.chen@email.wsu.edu>
2018-10-29 20:31:50 -0700

    Update index.php
    updated index

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,42 +5,8 @@
 		<title>
 			JavaScript Calculator : Casey Chen
 		</title>
-		<script>
-			function dispClear()
-			{
-				document.getElementById("calcFace").value = "";
-			}
-			
-			function placeInCalc(value)
-			{
-				document.getElementById("calcFace").value += value;
-			}
-			
-			function calculate()
-			{
-				var result = eval(document.getElementById('calcFace').value);
-				
-				document.getElementById("calcFace").value = result;
-			}
-			
-			function squareRoot()
-			{
-				var initial = eval(document.getElementById('calcFace').value);
-				var root = Math.sqrt(eval);
-				
-				document.getElementById("calcFace").value = root;
-			}
-		</script>
-		<style>
-			.calcBtns
-			{
-				width: 100%;
-				height: auto;
-				font-size: 3em;
-				text-align: center;
-				background: #FFE533;
-			}
-		</style>
+		<script src="/scripts/calcfx.js"></script>
+		<link rel="stylesheet" href="/styles/jscalc.css" />
 	</head>
 <body>
 	<div class="header">
@@ -49,29 +14,32 @@
 	</div>
 	<div class="content">
 		<h2>JavaScript Calculator</h2>
-		<table style="width: 50%; border: 2% solid #000000">
+		<table style="width: 50%" id="calcTbl">
 			<tr>
-				<td style="text-align: center" colspan="4">
-					<input type="text" id="calcFace" style="text-align: right; font-size: 3em; width: 100%" disabled value="1+2" />
+				<td style="text-align: center" colspan="5">
+					<input type="text" id="calcFace" disabled />
 				</td>
 			</tr>
-			<tr>
+			<tr class="trBtns">
 				<td>
-					<input type="button" value="%" class="calcBtns" />
+					<input type="button" value="%" class="calcBtns" onclick="placeInCalc('%')" />
 				</td>
 				<td>
-					<input type="button" value="sqrt" class="calcBtns" onclick="squareRoot()" />
+					<input type="button" value="&radic;" class="calcBtns" onclick="squareRoot()" />
 				</td>
 				<td>
-					<input type="button" value="sq" class="calcBtns" />
+					<input type="button" value="x²" class="calcBtns" onclick="square()" />
 				</td>
 				<td>
-					<input type="button" value="a" class="calcBtns" />
+					<input type="button" value="1/x" class="calcBtns" onclick="reciprocal()" />
+				</td>
+				<td>
+					<input type="button" value="&pi;" class="calcBtns" onclick="placeInCalc('3.1415926535')" />
 				</td>
 			</tr>
-			<tr>
+			<tr class="trBtns">
 				<td>
-					<input type="button" value="C" class="calcBtns" onclick="dispClear()" />
+					<input type="button" value="c" class="calcBtns" onclick="dispClear()" />
 				</td>
 				<td>
 					<input type="button" value="(" class="calcBtns" onclick="placeInCalc('(')" />
@@ -82,8 +50,11 @@
 				<td>
 					<input type="button" value="/" class="calcBtns" onclick="placeInCalc('/')" />
 				</td>
+				<td>
+					<input type="button" value="sin" class="calcBtns" onclick="trigFx('sine')" />
+				</td>
 			</tr>
-			<tr>
+			<tr class="trBtns">
 				<td>
 					<input type="button" value="7" class="calcBtns" onclick="placeInCalc('7')" />
 				</td>
@@ -96,8 +67,11 @@
 				<td>
 					<input type="button" value="*" class="calcBtns" onclick="placeInCalc('*')" />
 				</td>
+				<td>
+					<input type="button" value="cos" class="calcBtns" onclick="trigFx('cosine')" />
+				</td>
 			</tr>
-			<tr>
+			<tr class="trBtns">
 				<td>
 					<input type="button" value="4" class="calcBtns" onclick="placeInCalc('4')" />
 				</td>
@@ -110,8 +84,11 @@
 				<td>
 					<input type="button" value="-" class="calcBtns" onclick="placeInCalc('-')" />
 				</td>
+				<td>
+					<input type="button" value="tan" class="calcBtns" onclick="trigFx('tangent')" />
+				</td>
 			</tr>
-			<tr>
+			<tr class="trBtns">
 				<td>
 					<input type="button" value="1" class="calcBtns" onclick="placeInCalc('1')" />
 				</td>
@@ -124,10 +101,13 @@
 				<td>
 					<input type="button" value="+" class="calcBtns" onclick="placeInCalc('+')" />
 				</td>
-			</tr>
-			<tr>
 				<td>
-					<input type="button" value="a" class="calcBtns" />
+					<input type="button" value="n!" class="calcBtns" onclick="fact()" />
+				</td>
+			</tr>
+			<tr class="trBtns">
+				<td>
+					<input type="button" value="del" class="calcBtns" onclick="backspace()" />
 				</td>
 				<td>
 					<input type="button" value="0" class="calcBtns" onclick="placeInCalc('0')" />
@@ -135,162 +115,15 @@
 				<td>
 					<input type="button" value="." class="calcBtns" onclick="placeInCalc('.')" />
 				</td>
-				<td>
+				<td colspan="2">
 					<input type="button" value="=" class="calcBtns" onclick="calculate()" />
 				</td>
 			</tr>
 		</table>
+		<span style="color: #FF0000; font-weight: bold; width: 100%; text-align: center" id="errors">&nbsp;</span>
 	</div>
 	<div class="footer">
 		<?php include($_SERVER["DOCUMENT_ROOT"]."/includes/footer.php"); ?>
 	</div>
 </body>
-=======
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="/styles/main.css">
-		<title>
-			JavaScript Calculator : Casey Chen
-		</title>
-		<script>
-			function dispClear()
-			{
-				document.getElementById("calcFace").value = "";
-			}
-			
-			function placeInCalc(value)
-			{
-				document.getElementById("calcFace").value += value;
-			}
-			
-			function calculate()
-			{
-				var result = eval(document.getElementById('calcFace').value);
-				
-				document.getElementById("calcFace").value = result;
-			}
-			
-			function squareRoot()
-			{
-				var initial = eval(document.getElementById('calcFace').value);
-				var root = Math.sqrt(eval);
-				
-				document.getElementById("calcFace").value = root;
-			}
-		</script>
-		<style>
-			.calcBtns
-			{
-				width: 100%;
-				height: auto;
-				font-size: 3em;
-				text-align: center;
-				background: #FFE533;
-			}
-		</style>
-	</head>
-<body>
-	<div class="header">
-		<?php include($_SERVER["DOCUMENT_ROOT"]."/includes/header.php"); ?>
-	</div>
-	<div class="content">
-		<h2>JavaScript Calculator</h2>
-		<table style="width: 50%; border: 2% solid #000000">
-			<tr>
-				<td style="text-align: center" colspan="4">
-					<input type="text" id="calcFace" style="text-align: right; font-size: 3em; width: 100%" disabled value="1+2" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="button" value="%" class="calcBtns" />
-				</td>
-				<td>
-					<input type="button" value="sqrt" class="calcBtns" onclick="squareRoot()" />
-				</td>
-				<td>
-					<input type="button" value="sq" class="calcBtns" />
-				</td>
-				<td>
-					<input type="button" value="a" class="calcBtns" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="button" value="C" class="calcBtns" onclick="dispClear()" />
-				</td>
-				<td>
-					<input type="button" value="(" class="calcBtns" onclick="placeInCalc('(')" />
-				</td>
-				<td>
-					<input type="button" value=")" class="calcBtns" onclick="placeInCalc(')')" />
-				</td>
-				<td>
-					<input type="button" value="/" class="calcBtns" onclick="placeInCalc('/')" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="button" value="7" class="calcBtns" onclick="placeInCalc('7')" />
-				</td>
-				<td>
-					<input type="button" value="8" class="calcBtns" onclick="placeInCalc('8')" />
-				</td>
-				<td>
-					<input type="button" value="9" class="calcBtns" onclick="placeInCalc('9')" />
-				</td>
-				<td>
-					<input type="button" value="*" class="calcBtns" onclick="placeInCalc('*')" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="button" value="4" class="calcBtns" onclick="placeInCalc('4')" />
-				</td>
-				<td>
-					<input type="button" value="5" class="calcBtns" onclick="placeInCalc('5')" />
-				</td>
-				<td>
-					<input type="button" value="6" class="calcBtns" onclick="placeInCalc('6')" />
-				</td>
-				<td>
-					<input type="button" value="-" class="calcBtns" onclick="placeInCalc('-')" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="button" value="1" class="calcBtns" onclick="placeInCalc('1')" />
-				</td>
-				<td>
-					<input type="button" value="2" class="calcBtns" onclick="placeInCalc('2')" />
-				</td>
-				<td>
-					<input type="button" value="3" class="calcBtns" onclick="placeInCalc('3')" />
-				</td>
-				<td>
-					<input type="button" value="+" class="calcBtns" onclick="placeInCalc('+')" />
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<input type="button" value="a" class="calcBtns" />
-				</td>
-				<td>
-					<input type="button" value="0" class="calcBtns" onclick="placeInCalc('0')" />
-				</td>
-				<td>
-					<input type="button" value="." class="calcBtns" onclick="placeInCalc('.')" />
-				</td>
-				<td>
-					<input type="button" value="=" class="calcBtns" onclick="calculate()" />
-				</td>
-			</tr>
-		</table>
-	</div>
-	<div class="footer">
-		<?php include($_SERVER["DOCUMENT_ROOT"]."/includes/footer.php"); ?>
-	</div>
-</body>
->>>>>>> 250bffce5aead706ff70033b952ad4e1ac9461db
 </html>
